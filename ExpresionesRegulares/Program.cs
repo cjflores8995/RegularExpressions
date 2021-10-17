508adff876e4ee05ea4d775a50567cd13d54499a
= <carlos_jhousef593@hotmail.com>
2021-10-16 21:41:54 -0500

    First commit

---
+++ ExpresionesRegulares/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExpresionesRegulares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string strAlbaran = "A0800000000"; hasta B999999999;
+            //Console.WriteLine(ExpresionesRegulares.ValidateAlbaran.MethodValidateAlbaran(strAlbaran));
+
+            string strString = "www.blinblineo.net";
+            Console.WriteLine(ExpresionesRegulares.ValidateAlbaran.ValidarDominioWeb(strString));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExpresionesRegulares/ValidateAlbaran.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExpresionesRegulares
+{
+    public class ValidateAlbaran
+    {
+        public static bool MethodValidateAlbaran(string strString)
+        {
+            string strPattern = @"\A[AB]([0-9]{9})\Z";
+
+            return new Regex(strPattern).IsMatch(strString);
+        }
+
+        public static bool ValidatePatron(string strString)
+        {
+            //Validar que solo se pueda ingresar el patron: ABCD1720178068
+
+            //string strPattern = @"\A[a-z]{4}-([0-9]{10})\Z";
+
+            string strPattern = @"\A[AB]([0-9]{9})\Z";
+
+            System.Diagnostics.Trace.WriteLine(strString);
+
+            return new Regex(strPattern).IsMatch(strString);
+        }
+
+        public static bool ValidarNumeroTelefono(string strString)
+        {
+            //Validar 000 000 000
+
+            string strPattern = @"\A[0-9]{3} [0-9]{3} [0-9]{3}\Z";
+
+            return new Regex(strPattern).IsMatch(strString);
+        }
+
+        public static bool ValidarDominioWeb(string strString)
+        {
+            //Validar dominio web [www.nombredominio.tipodominio(.com, .net, .info, .org)]
+            string strPattern = @"\A[w]{3}(\.)([a-z0-9])+(\.)(com|net|info|org)";
+
+            return new Regex(strPattern).IsMatch(strString);
+        }
+    }
+}
